237955d09ce7af3469e53facda6f39ec2049b386
YeomeoR <YeomeoR@users.noreply.github.com>
2021-01-29 11:26:18 +0000

    logo png updates and Toggle

--- sq-gain/src/components/Toggle.js
+++ sq-gain/src/components/Toggle.js
@@ -5,7 +5,7 @@ const Toggle = ({ children, title }) => {
   const [toggle, setToggle] = useState(false);
   return (
     <motion.div layout className="position" onClick={() => setToggle(!toggle)}>
-      <motion.h4 layout >{title}<h6>Click here for more about our {title}...</h6></motion.h4>
+      <motion.h4 layout >{title}<h6><em>Click here for more about our {title}...</em></h6></motion.h4>
       
       {toggle ? children : ''}
       <div className="personnel-line"></div>
